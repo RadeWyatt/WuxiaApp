4331804ed212b98f04d99acd3ce435638026c30a
rpilkint <rade.pilkinton@wsu.edu>
2020-02-16 01:40:05 -0800

    can get next chapter

--- renderer.js
+++ renderer.js
@@ -1,20 +1,29 @@
-
-const novel = "https://www.wuxiaworld.com/novel/the-second-coming-of-gluttony/scog-chapter-1";
 const axios = require("axios");
 const cheerio = require("cheerio");
+
+var novel = "https://www.wuxiaworld.com/novel/the-second-coming-of-gluttony/scog-chapter-1";
+var myNext;
 
 const fetchData = async () => {
     const result = await axios.get(novel);
     return cheerio.load(result.data);
   };
 
-
-
 const getChapter = async() => {
-    const $ = await fetchData();
-    $('.chapter-nav').remove();
-    const chapter = $('#chapter-content').html();
+    const page = await fetchData();
+    page(".chapter-nav").first().remove();
+    var nextchapterlink = page(".chapter-nav").attr('href');
+    const chapter = page('#chapter-content').html();
     document.body.innerHTML = chapter;
+    myNext = novel.slice(0, 26) + nextchapterlink;
+    document.getElementsByClassName('chapter-nav')[0].removeAttribute('href');
+    document.getElementsByClassName('chapter-nav')[0].setAttribute('onclick', "nextChapter()");
 }
 
 getChapter();
+
+function nextChapter()
+{
+    novel = myNext;
+    getChapter();
+}
